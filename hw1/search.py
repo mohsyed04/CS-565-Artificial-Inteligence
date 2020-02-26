2a7dd7bf7ea508c6817e062438f7db21fcb6dbd8
mohsyed04 <syedmohiuddin004@gmail.com>
2020-02-26 14:25:46 -0500

    Add files via upload

--- hw1/search.py
+++ hw1/search.py
@@ -72,24 +72,18 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
-
-def getPath(target, parent, actions,startState):
-    stack = []
-    path = []
-    current_state = target #target is a state tuple
-    while (current_state != startState):#get the path
-        stack.append(actions[current_state])
-        print("appended")
-        print(actions[current_state])
-        current_state = parent[current_state]
-        print("changed state")
-        print(current_state) 
+'''to reverse the reversed path, first push all the values into a stack and pop and save the values in another list to get the correct order'''
+def getPath(target, parent, actions,startState): #accepts the goal/target, parent dictionary, actionsa dictionary and the start state
+    stack = []  #list as stack
+    path = []   #to hold the path in correct order
+    current_state = target #initialize the surrent state to the goal state and traverse back to buid a reveresed path. target is a state tuple
+    while (current_state != startState): #loop until you hit the start state
+        stack.append(actions[current_state]) 
+        current_state = parent[current_state] #move back to parent state
 
     while (len(stack) > 0): #reverse the path to get in right order
         action = stack.pop()
         path.append(action)
-    for i in path:
-        print(i)
     return path
 
 def depthFirstSearch(problem):
@@ -109,48 +103,38 @@ def depthFirstSearch(problem):
     "*** YOUR CODE HERE ***"
     stack=util.Stack()
     path = []
-    start_state = (problem.getStartState(), [],0) #get start state
+    start_state = (problem.getStartState(), [],0) #get start state and add empty action and cost to get to the start state before pushing on stack
 
-    if problem.isGoalState(start_state[0]):
+    if problem.isGoalState(start_state[0]): #if the start state is a goal state, return an empty list of actions
         print("goal reached")
         return path
 
-    visited = []       
-    parent = {}
-    actions = {}
-    stack.push(start_state)
+    visited = []    #list to hold visited states
+    parent = {}     #to save the (state: parent state/node) pairs
+    actions = {}    #to save the (state: action to get there)  pairs
+    stack.push(start_state) #each stack item is of format (state,action to get to the state, cost to get to the state )
     
     
     while(stack.isEmpty() == False): #loop until stack is empty
         
         item = stack.pop() # pop an item from the stack
-        #print("length of item:")
-        #print(len(item))
 
-        if item[0] not in visited:
+        if item[0] not in visited: #check the visited list before processing, if visited, ignore the state. item[0] means state of the item
             if problem.isGoalState(item[0]): #if its a goal state, add to the solution and return
-                ##call a function to reverse the path and create a list
-                print("Goal state:", item[0])
-                print("Start state:", start_state)
-                path = getPath(item[0],parent,actions,start_state[0])
-                print("returning from 1")
-                return path
+                path = getPath(item[0],parent,actions,start_state[0]) ##call a function to create a path in reverse order and return the path list in correct order
+                return path 
 
-            visited.append(item[0]) #else append it to the visited list and
+            visited.append(item[0]) #else append it to the visited list and do:
 
-            successors = problem.getSuccessors(item[0]) #push its successors onto the stack
-            for scr in successors:
-                if scr[0] not in visited:
-                    stack.push((scr[0],scr[1],scr[2]))
-                    parent[scr[0]] = item[0] #scr(0) is the state and scr(1) is the action it took to get to state scr[0]
-                    print(scr[0],scr[1])
-                    actions[scr[0]] = scr[1]
+            successors = problem.getSuccessors(item[0]) #push all the of its successors onto the stack
+            for scr in successors: #for each successor of the current state
+                if scr[0] not in visited: #check if the current successor is already visited, if not, do:
+                    stack.push((scr[0],scr[1],scr[2])) #push the successor in the same stack item format of (state,action,cost)
+                    #scr(0) is the state and scr(1) is the action required to get to state scr[0]
+                    parent[scr[0]] = item[0] # save the parent state (item[0]) of the current state/successor [scr[0]] in the parent dictionary 
+                    actions[scr[0]] = scr[1] #save the action reuired (scr[1]) to get to the current state/successor  (scr[0])
 
-
-    print("returning from 2")
-    '''for key,value in parent:
-        print(key, value)'''
-    return path
+    return path #if goal is not found return an empty list
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
